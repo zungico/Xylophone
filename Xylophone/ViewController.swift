564a981549ed13f1e6ded7f6cbef7bd4af611106
Vladimir Alekseev <vovauu@icloud.com>
2023-01-28 03:40:18 +0300

    fix sounds

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -15,27 +15,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
-    var player: AVAudioPlayer?
+    var player: AVAudioPlayer!
 
     func playSound(_ sound: String?) {
-        guard let url = Bundle.main.url(forResource: sound, withExtension: "wav") else { return }
+        let url = Bundle.main.url(forResource: sound, withExtension: "wav")
+        player = try!AVAudioPlayer(contentsOf: url!)
+        player.play()
 
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-
-            guard let player = player else { return }
-
-            player.play()
-
-        } catch let error {
-            print(error.localizedDescription)
-        }
     }
     @IBAction func buttonPressed(_ sender: UIButton) {
-        playSound(sender.currentTitle)
+        playSound(sender.titleLabel?.text)
         sender.alpha = 0.5
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
             sender.alpha = 1
